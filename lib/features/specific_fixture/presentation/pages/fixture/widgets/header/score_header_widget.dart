e63b675f14a31625b0f2171c386e0e7162e37fd1
fylyppo <63547653+fylyppo@users.noreply.github.com>
2022-04-17 11:23:25 +0200

    Recognize if the match is live in score header widget

--- lib/features/specific_fixture/presentation/pages/fixture/widgets/header/score_header_widget.dart
+++ lib/features/specific_fixture/presentation/pages/fixture/widgets/header/score_header_widget.dart
@@ -4,15 +4,14 @@ import 'package:flutter/material.dart';
 import '../../../../../domain/entities/fixture.dart';
 import 'elapsed_time_header_widget.dart';
 
-
 class ScoreHeaderWidget extends StatelessWidget {
   final Fixture fixture;
   final double shrinkOffset;
-  const ScoreHeaderWidget(
-      {Key? key,
-      required this.fixture,
-      required this.shrinkOffset})
+  ScoreHeaderWidget(
+      {Key? key, required this.fixture, required this.shrinkOffset})
       : super(key: key);
+
+  List<String> liveStatus = ['1H', 'HT', '2H', 'ET', 'P', 'LIVE'];
 
   @override
   Widget build(BuildContext context) {
@@ -20,16 +19,20 @@ class ScoreHeaderWidget extends StatelessWidget {
       height: shrinkOffset < 100 ? 70 : max(70 - shrinkOffset + 100, 50),
       color: Colors.grey[900],
       width: 100,
-      child: Center(
-          child: Column(
-        children: [
-          Text(
-            fixture.goals.home.toString() + ' : ' + fixture.goals.away.toString(),
-            style: const TextStyle(fontSize: 42, fontWeight: FontWeight.bold),
-          ),
-          ElapsedTimeWidget(status: fixture.fixture.status, shrinkOffset: shrinkOffset),
+      child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+      Text(
+        fixture.goals.home.toString() +
+            ' : ' +
+            fixture.goals.away.toString(),
+        style: const TextStyle(fontSize: 42, fontWeight: FontWeight.bold),
+      ),
+      if (liveStatus.contains(fixture.fixture.status.short))
+        ElapsedTimeWidget(
+            status: fixture.fixture.status, shrinkOffset: shrinkOffset),
         ],
-      )),
+      ),
     );
   }
 }
